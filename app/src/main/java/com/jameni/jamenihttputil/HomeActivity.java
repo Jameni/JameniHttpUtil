291c2d5728ca8deb53f08123031adca969325401
1607613241@qq.com <1607613241@qq.com>
2018-10-08 16:47:10 +0800

    新建项目

---
+++ app/src/main/java/com/jameni/jamenihttputil/HomeActivity.java
@@ -0,0 +1,18 @@
+package com.jameni.jamenihttputil;
+
+import android.support.v7.app.AppCompatActivity;
+
+import org.androidannotations.annotations.AfterViews;
+import org.androidannotations.annotations.EActivity;
+
+@EActivity(R.layout.page_home)
+public class HomeActivity extends AppCompatActivity {
+
+
+
+    @AfterViews
+    void initPage() {
+
+
+    }
+}
---
+++ app/src/main/java/com/jameni/jamenihttputil/MainActivity.java
@@ -0,0 +1,15 @@
+package com.jameni.jamenihttputil;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        HomeActivity_.intent(this).start();
+        finish();
+    }
+}
